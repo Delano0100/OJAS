1417b303c56eb698155875a994f390c49fefab64
Himanshu <148736103+Delano0100@users.noreply.github.com>
2026-05-01 16:51:32 +0530

    Update handshake.routes.js

--- ojas-backend/src/routes/handshake.routes.js
+++ ojas-backend/src/routes/handshake.routes.js
@@ -164,7 +164,7 @@ mqttClient.publish(PUBLISH_TOPIC, rawBuffer, { qos: 1 }, (err) => {
         })
 
     // ── Step 4: Return response ───────────────────────────────────────────────
-    return res.status(200).json(dlmsDecodeResponse.data)
+     return res.status(200).json({'voltage': dlmsDecodeResponse.data.value/1000000})
     
 
   } catch (error) {
